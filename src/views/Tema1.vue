265a01d06b40b2190e9d324eb61e9d965f5691ee
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-08-04 18:25:34 -0500

    assets tema 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,7 +5,18 @@
       .titulo-principal.color-acento-contenido
         .titulo-principal__numero
           span 1
-        h1 Titulo de primer nivel
+        h1 Objetivos de desarrollo sostenible (sostenibilidad)
+
+      p.mb-4 Los Objetivos de Desarrollo Sostenible (ODS) constituyen un marco global orientado a equilibrar el crecimiento económico, la inclusión social y la protección ambiental. 
+
+      .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5
+        .bloque-texto-g__img(
+          :style="{'background-image': `url(${require_src('@/assets/curso/tema1/1.png')})`}"
+        )
+        .bloque-texto-g__texto.p-4
+          p.mb-0 En el contexto de los negocios sostenibles, estos objetivos permiten comprender cómo las decisiones empresariales y de consumo impactan el entorno, su estudio facilita la identificación de oportunidades comerciales responsables, alineando las estrategias de mercado con principios de sostenibilidad aplicables a diferentes sectores productivos.
+
+
         
 
   </template>
